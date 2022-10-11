5e800c1d0c114e5b96d263aa3fbf1d6cbc7c8423
Han chaeeun <gkscodms99@naver.com>
2022-10-11 17:34:55 +0900

    Update module

--- module/load_module.py
+++ module/load_module.py
@@ -6,15 +6,24 @@ import numpy as np
 import pandas as pd
 
 # Loading PV Data in Jinju and Gangneung
-def Load_PV(path):
-  file_list = os.listdir(path)
+def Load_PV_Jindo(path):
+  file_list = [x for x in os.listdir(path) if x != '.ipynb_checkpoints' ]
 
   result_df = pd.DataFrame()
   for i in file_list:
     df = pd.read_csv(path+i, encoding='CP949')
-    df['년도'] = pd.to_datetime(df['년월일']).dt.year
+    year = i[-12:-8]
+    df['년도'] = year
+    df.dropna(how='all', axis=1, inplace=True)
+    cols = [x for x in df.columns if x not in ['합계', '발전기코드', '발전기명', '충전_방전', '발전_펌핑구분', '년도', '날짜']]
+    for i in cols:
+      rname = re.sub(r'[^0-9]',"",i)
+      df.rename(columns = {i:str(rname)}, inplace=True)
+
     result_df = pd.concat([result_df, df], axis=0)
-  result_df.drop(columns=['호기', '구분', '총량', '평균', '최대', '최소', '최대(시간별)', '최소(시간별)'], inplace=True)  
+  result_df.drop(columns=['합계', '발전기코드', '발전기명', '충전_방전', '발전_펌핑구분'], inplace=True)
+  result_df.sort_values(by=['년도','날짜'], inplace=True)
+  result_df.reset_index(drop=True, inplace=True)
   return result_df
 
 
@@ -29,12 +38,12 @@ def Load_PV_Mokpo(path):
     df['년도'] = np.int(year)
     df.dropna(how='all', axis=1, inplace=True)
 
-    cols = [x for x in df.columns if x not in [' 계 ','년도','월','일']]
+    cols = [x for x in df.columns if x not in ['계', ' 계 ','년도','월','일']]
     for i in cols:
       rname = re.sub(r'[^0-9]',"",i)
       df.rename(columns = {i:np.str(np.int(rname))}, inplace=True)
     result_df = pd.concat([result_df, df], axis=0)
-  result_df.drop(columns=' 계 ', inplace=True)
+  # result_df.drop(columns=' 계 ', inplace=True)
   return result_df
 
 
@@ -62,26 +71,37 @@ def Load_ASOS(path):
 
 
 # Loading Particulate Data
-def Load_PM(path, name, area):
-  file_list = os.listdir(path)
+def Load_PM(name, area, years):
+  path = '/content/gdrive/MyDrive/SolarPower/PMdata/'
   PMdata = pd.DataFrame()
-  for i in file_list:
-    p = pathlib.Path(path+i)
-    Extension = p.suffix
-    if Extension == '.xlsx':
-      data = pd.read_excel(p)
-    else:
-      try:
-        data = pd.read_csv(p, encoding = 'CP949')
-      except:
-        data = pd.read_csv(p, encoding = 'utf8')
-    data = data[data[name].isin(area)]
-    PMdata = pd.concat([PMdata, data]).reset_index(drop=True)
-
+  
+  for i in years:
+    file_path = f'{path}{i}/'
+    print(file_path)
+    file_list = os.listdir(file_path)
+    for i in file_list[:2]:
+      p = pathlib.Path(file_path+i)
+      Extension = p.suffix
+      if Extension == '.xlsx':
+        data = pd.read_excel(p)
+      else:
+        try:
+          data = pd.read_csv(p, encoding = 'CP949')
+        except:
+          data = pd.read_csv(p, encoding = 'utf8')
+      data = data[data[name].isin(area)]
+      PMdata = pd.concat([PMdata, data]).reset_index(drop=True)
+  
   PMdata['시간'] = PMdata['측정일시'].astype(str).str[-2:]
   PMdata['시간'] = PMdata['시간'].astype(int)
   PMdata['일자'] = PMdata['측정일시'].astype(str).str[:-2]
   PMdata['년도'] = PMdata['측정일시'].astype(str).str[:4]
 
-  PMdata.sort_values(['일자'], inplace=True)
-  return PMdata
+  return PMdata   
+  
+def Format_PV(df, date, id_cols):
+  data = df.copy()
+  data = data.melt(id_vars= id_cols , var_name='시간', value_name='발전량')
+  data['시간'] = data['시간'].astype(int)
+  data.reset_index(drop=True, inplace=True)
+  return data
